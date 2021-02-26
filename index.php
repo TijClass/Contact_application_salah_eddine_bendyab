67f4364094d26e56a94b8064a081c938a2e98f57
salahhusa9 <salahhusa9@gmail.com>
2021-02-26 20:33:38 +0100

    add routing system creaction

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
     $myRoutes=[
-        'login' => 'view/login.php',
-        'home'  => 'view/home.php',
-        'test'  => 'view/one/tow.php'
+        'login' => 'view\login.php',
+        'home'  => 'view\home.php',
+        'test'  => 'view\one\tow.php'
     ];
     // get : http://localhost:3030/one/tow?key=val
     $linkReq=$_SERVER['REQUEST_URI'];   // get : /one/tow?key=val
@@ -11,12 +11,8 @@
     $path=explode("?",$linkReq);        // separt the get parame
     // print_r($path);
     if (isset($myRoutes[$path[0]])) {
-        if (isset($path[1])) {
-            echo $myRoutes[$path[0]]."?".$path[1]; //routing
-        }else{
-            echo $myRoutes[$path[0]]; //routing
-        }
+        include $myRoutes[$path[0]]; //routing
     }else{
-        return header("HTTP/1.0 404 Not Found");;
+        return header("HTTP/1.0 404 Not Found");
     }
 ?>
