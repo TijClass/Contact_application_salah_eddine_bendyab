6d0a15a474ad7b3849a06e6428c1cded9677d838
salahhusa9 <salahhusa9@gmail.com>
2021-02-28 14:34:29 +0100

    logout creaction

--- view/logout.php
+++ view/logout.php
@@ -1,5 +1,9 @@
 <?php
     include('./components/header.php');
+    if (getSession('login') === false) {
+        header("location:login");
+        die;
+    }
     CreateSession('login',false);
     CreateSession('user','');
     header("location:login");
